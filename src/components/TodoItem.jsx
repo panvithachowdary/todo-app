081d9835c34b19c957cf80e3781f57906c731980
Panvitha Chowdary Murakonda <panvithachowdary@gmail.com>
2026-05-22 16:52:38 +0530

    Update TodoItem.jsx

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,15 +1,19 @@
 function TodoItem({ todo, toggleTodo, deleteTodo }) {
-    return (
-      <div className="item">
-        <span
-          onClick={() => toggleTodo(todo.id)}
-          className={todo.completed ? "done" : ""}
-        >
-          {todo.text}
-        </span>
-        <button onClick={() => deleteTodo(todo.id)}>X</button>
-      </div>
-    );
-  }
-  
-  export default TodoItem;
+  return (
+    <div className="item">
+      <span
+        className={todo.completed ? "done" : ""}
+        onClick={() => toggleTodo(todo.id)}
+      >
+        {todo.completed ? "✅ " : "📌 "}
+        {todo.text}
+      </span>
+
+      <button onClick={() => deleteTodo(todo.id)}>
+        Delete
+      </button>
+    </div>
+  );
+}
+
+export default TodoItem;
